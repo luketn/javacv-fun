9660a8b3fd3ad359074ecad5de63c7ac263e08df
Luke Thompson <lthompson@infomedia.com.au>
2015-05-10 12:21:26 +1000

    Add mouse click handler to the jFrame

--- src/main/java/com/mycodefu/javacv/fun/display/DisplayImages.java
+++ src/main/java/com/mycodefu/javacv/fun/display/DisplayImages.java
@@ -5,7 +5,11 @@ import org.opencv.core.Mat;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
+import java.util.stream.Collectors;
+import java.util.stream.DoubleStream;
 
 import static java.lang.Math.max;
 
@@ -19,6 +23,7 @@ import static java.lang.Math.max;
 public class DisplayImages {
     protected JFrame frame;
     private final MatrixToBufferedImage converter;
+    private Mat currentImage;
 
     public DisplayImages() {
         frame = null;
@@ -27,6 +32,8 @@ public class DisplayImages {
 
     public void draw(Mat imageMatrix) {
         initializeWindow(imageMatrix.width(), imageMatrix.height());
+
+        currentImage = imageMatrix;
 
         final BufferedImage image = converter.convert(imageMatrix);
 
@@ -40,6 +47,36 @@ public class DisplayImages {
             frame = new JFrame();
             frame.setBounds(0, 0, max(width, screenSize.width), max(height, screenSize.height));
             frame.setVisible(true);
+
+            frame.addMouseListener(new MouseListener() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    final double[] pixel = currentImage.get(e.getY(), e.getX());
+                    final String pixelString = DoubleStream.of(pixel).mapToObj(Double::toString).collect(Collectors.joining(","));
+
+                    System.out.println("Mouse clicked! " + e + ", " + pixelString);
+                }
+
+                @Override
+                public void mousePressed(MouseEvent e) {
+
+                }
+
+                @Override
+                public void mouseReleased(MouseEvent e) {
+
+                }
+
+                @Override
+                public void mouseEntered(MouseEvent e) {
+
+                }
+
+                @Override
+                public void mouseExited(MouseEvent e) {
+
+                }
+            });
         }
     }
 
